732d75c2bbe23d271d492c043dcce04427211df2
Linda1997382 <159412558+Linda1997382@users.noreply.github.com>
2025-10-25 19:11:52 -0600

    Arreglos problema puerto

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const alcoholRuleService = require('./services/AlcoholRuleService');
 const alcoholRoutes = require('./routes/alcohol');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
